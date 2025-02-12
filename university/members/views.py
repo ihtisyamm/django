80d3d449ccd235e323166a45765105d71c1b65bf
Muhammad Ihtisyam <ihtisyam@Icam.local>
2025-02-12 11:49:56 +0000

    Re-initialised continue in another machine

--- university/members/views.py
+++ university/members/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-    template = loader.get_template('myfirst.html')
+    template = loader.get_template('index.html')
     return HttpResponse(template.render())
